1cb92fb16e00641a56d9b8ad694db664a29294ca
gaojieqi <gaojieqi888@163.com>
2016-10-04 23:19:06 +0800

    10.4

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
 from sklearn import svm
 from sklearn.svm import SVC
 from sklearn import datasets
+import matplotlib.pyplot as plt
+import cv2
 
 digits=datasets.load_digits()
-print digits.values()
-print digits.target
-print digits.
+clf=svm.SVC(gamma=0.001,C=100)
+'''
+1.while a high C aims at classifying all training examples correctly
+2.The larger gamma is, the closer other examples must be to be affected.
+'''
+print len(digits.data)
+
+x,y=digits.data[:-1],digits.target[:-1]
+clf.fit(x,y)
+clf.predict()
+plt.imshow(digits.images[-2],cmap=plt.cm.gray_r,interpolation="nearest")
+plt.show()
 
 
-x=[]
-for i in range(5):
-    x.append([i])
-y=[]
-for i in range(5):
-    y.append(i)
+#raw_input()
 
-
-
-#raw_input()cv2.imwrite( "d:/123.png", colorCVT );
+# cv2.imwrite( "d:/123.png", colorCVT );
 
 
