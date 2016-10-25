495e5ba6c2346b379c207a3e0d9c1e3ecd1bd98e
gaojieqi <gaojieqi888@163.com>
2016-10-25 10:43:22 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ from matplotlib import pyplot as plt
 import DetectChars
 import DetectPlates
 import PossibleChar
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
 
 def compare(list_element,element):
@@ -126,9 +128,50 @@ def find_num_img(img_to_find,separate_line,quantity):
                     gray[k][j]=255
     cv2.imwrite("gray_return.png", gray)
 
+def neural_network_model(data):
+    hidden_1_layer={'weights':tf.Variable(tf.random_normal([feature_num,n_nodes_hl1])),
+                    'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+    hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
+    hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
+    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3,n_classes])),
+                      'biases': tf.Variable(tf.random_normal([n_classes]))}
+    l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']),hidden_1_layer['biases'])
+    l1 = tf.nn.relu(l1)
+    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']) , hidden_2_layer['biases'])
+    l2 = tf.nn.relu(l2)
+    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']) , hidden_3_layer['biases'])
+    l3 = tf.nn.relu(l3)
+    output = tf.add(tf.matmul(l3, output_layer['weights']) , output_layer['biases'])
+    return output
 
 
-img=cv2.imread('test.jpg')
+def train_neural_network(x):
+    prediction=neural_network_model(x)
+    cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction,y_train))
+    optimizer=tf.train.AdamOptimizer().minimize(cost)
+    hm_epochs=10
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+        for epoch in range(hm_epochs):
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x,epoch_y=mnist.train.next_batch(batch_size)
+                _,c=sess.run([optimizer,cost],feed_dict={x_train:epoch_x,y_train:epoch_y})
+
+
+
+
+
+
+
+
+
+
+
+
+
+img=cv2.imread('test1.jpg')
 #Filt out some noise
 img = cv2.bilateralFilter(img, 11, 17, 17)
 #detect the boarder
@@ -190,6 +233,7 @@ dst = np.array([
 	[0, maxHeight - 1]], dtype = "float32")
 M = cv2.getPerspectiveTransform(rect, dst)
 warp = cv2.warpPerspective(img, M, (maxWidth,maxHeight))
+warp = cv2.bilateralFilter(warp, 11, 17, 17)
 gray = cv2.cvtColor(warp, cv2.COLOR_RGB2GRAY)
 cv2.imwrite("warp.png",warp)
 
@@ -262,36 +306,64 @@ cv2.imwrite("warp.png",warp)
 
 # clf.fit(X_train,y_train)
 # joblib.dump(clf, "svm_model.m")
-# # clf = joblib.load("svm_model.m")
-# print clf.predict(X_train[10000].reshape(1,784)),y_train[10000]
+# clf = joblib.load("svm_model.m")
+# print clf.score(X_test.reshape(10000,784),y_test)
+
+
 
 '''kNearest'''
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-X_train=X_train.reshape(60000,784).astype(np.float32)
-y_train=y_train.astype(np.float32)
-kNearest = cv2.ml.KNearest_create()
-kNearest.train(X_train,cv2.ml.ROW_SAMPLE,y_train)
+# (X_train, y_train), (X_test, y_test) = mnist.load_data()
+# X_train=X_train.reshape(60000,784).astype(np.float32)
+# y_train=y_train.astype(np.float32)
+# kNearest = cv2.ml.KNearest_create()
+# kNearest.train(X_train,cv2.ml.ROW_SAMPLE,y_train)
+
+
+'''Neural Network'''
+mnist= input_data.read_data_sets("/tmp/data/",one_hot=True)
+sess=tf.InteractiveSession()
+n_nodes_hl1=500
+n_nodes_hl2=500
+n_nodes_hl3=500
+n_classes=10
+batch_size=100
+feature_num=784
+x_train=tf.placeholder('float',[None,feature_num])
+y_train=tf.placeholder('float')
+train_neural_network(x_train)
+x_test=tf.placeholder('float',[None,feature_num])
+y_test=tf.placeholder('float')
+correct=tf.equal(tf.argmax(x_test,1),tf.argmax(y_test,1))
+accuracy=tf.reduce_mean(tf.cast(correct,'float'))
+print('Accuracy:',accuracy.eval({x_test:mnist.test.images,y_test:mnist.test.labels}))
 
 
 
-'''use testimg to test the accuracy of the programme'''
+
+'''use testimg to test the accuracy of the programme ***factor_h needs to be 0.3 ***'''
 img = cv2.imread('testimg.png')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret,thresh=cv2.threshold(gray,50,255,cv2.THRESH_BINARY_INV)
 
 
-ret,thresh=cv2.threshold(gray,150,255,cv2.THRESH_BINARY_INV)
+
+thresh=cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11, 2)
 cv2.imwrite('thresh.jpg',thresh)
+
+
 img_find_contour,contours,hierarchy= cv2.findContours(thresh.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-out = np.zeros(gray.shape,np.uint8)
+out = np.zeros(img.shape,np.uint8)
 H,W=gray.shape
+
 
 '''factor that makes the digits to the center of the image'''
 factor_h=0.3
 factor_w=0.1
+
 '''minimal h of digits's hight'''
 H_MIN=28
 
+'''Flag that distinct digits from aphalet'''
+FLAG=100
 
 blnKNNTrainingSuccessful = DetectChars.loadKNNDataAndTrainKNN()         # attempt KNN training
 listOfPossibleChars = []
@@ -299,9 +371,10 @@ listOfPossibleChars = []
 
 for cnt in contours:
         [x,y,w,h] = cv2.boundingRect(cnt)
-        # if  h>H_MIN and w<0.5*W and h>H*0.5:#use in ordinary circumstance
-        if h>H_MIN:#use in testing
-            cv2.rectangle(warp,(x,y),(x+w,y+h),(0,255,0),2)
+        # if (w>0.1*W) and (w<0.3*W) and (h>H*0.5) :            #use in ordinary circumstance
+        if h>H_MIN:                                             #use in testing
+            cv2.rectangle(warp,(x,y),(x+w,y+h),(0,255,255),2)
+            cv2.imwrite('warp.png', warp)
             num = thresh[(y-int(factor_h*h)):(y+h+int(h*factor_h)),(x-int(factor_w*w)):(x+w+int(w*factor_w))]
             # num= thresh[y:(y + h ), x :(x + w)]
             H1,W1=num.shape
@@ -311,6 +384,7 @@ for cnt in contours:
                         num[j,i]=0
             # plt.imshow(num, 'gray')
             # plt.show()
+
             num_possible = PossibleChar.PossibleChar(num)
             # listOfPossibleChars.append(possibleChar)
             # listOfListsOfMatchingCharsInPlate=DetectChars.findListOfListsOfMatchingChars(listOfPossibleChars)
@@ -333,9 +407,14 @@ for cnt in contours:
             if num_possible.fltAspectRatio != 0:
                 num_possible=DetectChars.removeInnerOverlappingChars(num_possible)
                 num_detect = DetectChars.recognizeCharsInPlate(num, num_possible)
-                if num_detect!=100:
+
+                # ---------------------------dataset 1 ---------------------------------
+                if num_detect!=FLAG:
                     cv2.putText(out, str(num_detect), (x, y + h), 0, 1, (0, 255, 0))
                     print num_detect
+                    cv2.imwrite('out.png', out)
+
+                # ---------------------------dataset 2  Knearest ---------------------------------
                 else:
                     numsmall = cv2.resize(num, (28, 28))
                     ret, numsmall = cv2.threshold(numsmall, 1, 255, cv2.THRESH_BINARY)
@@ -344,13 +423,17 @@ for cnt in contours:
                     retval, npaResults, neigh_resp, dists = kNearest.findNearest(numsmall, k=1)
                     cv2.putText(out, str(int(npaResults[0][0])), (x, y + h), 0, 1, (0, 255, 0))
                     print int(npaResults[0][0])
+                    cv2.imwrite('out.png', out)
 
-            # print clf.predict(numsmall)
+                # ---------------------------dataset 2  SVM ---------------------------------
+                # else:
+                #     numsmall = cv2.resize(num, (28, 28))
+                #     ret, numsmall = cv2.threshold(numsmall, 1, 255, cv2.THRESH_BINARY)
+                #     numsmall = numsmall.reshape((1, 784))
+                #     result=clf.predict(numsmall)
+                #     cv2.putText(out, str(result), (x, y + h), 0, 1, (0, 255, 0))
+                #     cv2.imwrite('out.png', out)
 
-cv2.imwrite('out.png',out)
 
 
-
-#raw_input()
-# cv2.imwrite( "d:/123.png", colorCVT );
 
